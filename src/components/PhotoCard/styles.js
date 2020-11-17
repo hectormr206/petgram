91414c48c45ee34cb07451a0a0ef878d8656726e
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-11-17 08:06:55 -0600

    Creating animations with keyframes

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
+${fadeIn({ time: '1.2s' })}
  border-radius: 10px;
  display: block;
  height: 0;
@@ -11,6 +13,7 @@ export const ImgWrapper = styled.div`
 `
 
 export const Img = styled.img`
+  
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
   height: 100%;
   object-fit: cover;
