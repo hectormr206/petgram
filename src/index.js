1d07725a1b3958571f63b08f3d5d5b15d9642efc
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-11-19 22:18:10 -0600

    Initializing React Apollo Client for GraphQL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+// Dependecies
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+// Components
 import { App } from './App'
 
-ReactDOM.render(<App />, 
-document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-hectormr206.vercel.app/graphql'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'))
