be00cce6cc699bb46c869e2862a7231b4789c40e
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-11-20 10:47:50 -0600

    Create Hook useGetPhotos

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,30 +1,16 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { useGetPhotos } from '../../hooks/useGetPhotos'
 
-import { useQuery, gql } from '@apollo/client'
-
-const getPhotos = gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`
-
-export const ListOfPhotoCards = () => {
-  const { loading, error, data } = useQuery(getPhotos)
+export const ListOfPhotoCards = ({ categoryId }) => {
+  const { loading, error, data } = useGetPhotos(categoryId)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
   return (
     <ul>
-      {data.photos.map(photo => <PhotoCard key={photo.id} id={photo.id} likes={photo.likes} src={photo.src} />)}
+      {data.photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
