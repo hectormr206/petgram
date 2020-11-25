a9d7ccc2d9aaf1dc17aac21aec89a02896500fa1
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-11-24 23:50:31 -0600

    Like as a registered user

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Article, ImgWrapper, Img } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { useToggleLikeMutation } from '../../hooks/useToggleLikeMutation'
 import { FavButton } from '../FavButton'
@@ -8,19 +7,16 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   const { toggleLikeMutation } = useToggleLikeMutation()
   const handleFavClick = () => {
-    !liked && toggleLikeMutation({
+    toggleLikeMutation({
       variables: {
         input: { id }
       }
     })
-    setLiked(!liked)
   }
 
   return (
