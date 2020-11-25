bc78575c3765691cea67c3e93b9f3bdd0a749ccd
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-11-24 22:58:37 -0600

    Creating the login

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,26 +2,36 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { useRegisterMutation } from '../hooks/useRegisterMutation'
+import { useLoginMutation } from '../hooks/useLoginMutation'
 
 export const NotRegisteredUser = () => {
-  const { registerMutation, data, loading, error } = useRegisterMutation()
+  const { registerMutation, loading: loadingRegister, error: errorRegister } = useRegisterMutation()
+  const { loginMutation, loading: loadingLoding, error: errorLoading } = useLoginMutation()
 
   return (
     <Context.Consumer>
       {
         ({ activateAuth }) => {
-          const onSubmit = ({ email, password }) => {
+          const onSubmitRegister = ({ email, password }) => {
             const input = { email, password }
             const variables = { input }
             registerMutation({ variables }).then(activateAuth)
           }
 
-          const errorMsg = error && 'El usuario ya existe o hay algún problema.'
+          const onSubmitLogin = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            loginMutation({ variables }).then(activateAuth)
+          }
+
+          const errorMsgRegister = errorRegister && 'El usuario ya existe o hay algún problema.'
+
+          const errorMsgLogin = errorLoading && 'La contraseña no es correcta o el usuario no existe.'
 
           return (
             <>
-              <UserForm onSubmit={onSubmit} error={errorMsg} title='Registrate' disabled={loading} />
-              <UserForm onSubmit={activateAuth} title='Iniciar sesión' />
+              <UserForm onSubmit={onSubmitRegister} error={errorMsgRegister} title='Registrate' disabled={loadingRegister} />
+              <UserForm onSubmit={onSubmitLogin} error={errorMsgLogin} title='Iniciar sesión' disabled={loadingLoding} />
             </>
           )
         }
