2fc8ed2545915321d7b2e1f3e963f7ba0613e563
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-11-24 23:14:01 -0600

    Persisting data in Session Storage

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,41 +1,40 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { useRegisterMutation } from '../hooks/useRegisterMutation'
 import { useLoginMutation } from '../hooks/useLoginMutation'
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
   const { registerMutation, loading: loadingRegister, error: errorRegister } = useRegisterMutation()
   const { loginMutation, loading: loadingLoding, error: errorLoading } = useLoginMutation()
 
+  const onSubmitRegister = ({ email, password }) => {
+    const input = { email, password }
+    const variables = { input }
+    registerMutation({ variables }).then(({ data }) => {
+      const { signup } = data
+      activateAuth(signup)
+    })
+  }
+
+  const onSubmitLogin = ({ email, password }) => {
+    const input = { email, password }
+    const variables = { input }
+    loginMutation({ variables }).then(({ data }) => {
+      const { login } = data
+      activateAuth(login)
+    })
+  }
+
+  const errorMsgRegister = errorRegister && 'El usuario ya existe o hay algún problema.'
+
+  const errorMsgLogin = errorLoading && 'La contraseña no es correcta o el usuario no existe.'
+
   return (
-    <Context.Consumer>
-      {
-        ({ activateAuth }) => {
-          const onSubmitRegister = ({ email, password }) => {
-            const input = { email, password }
-            const variables = { input }
-            registerMutation({ variables }).then(activateAuth)
-          }
-
-          const onSubmitLogin = ({ email, password }) => {
-            const input = { email, password }
-            const variables = { input }
-            loginMutation({ variables }).then(activateAuth)
-          }
-
-          const errorMsgRegister = errorRegister && 'El usuario ya existe o hay algún problema.'
-
-          const errorMsgLogin = errorLoading && 'La contraseña no es correcta o el usuario no existe.'
-
-          return (
-            <>
-              <UserForm onSubmit={onSubmitRegister} error={errorMsgRegister} title='Registrate' disabled={loadingRegister} />
-              <UserForm onSubmit={onSubmitLogin} error={errorMsgLogin} title='Iniciar sesión' disabled={loadingLoding} />
-            </>
-          )
-        }
-      }
-    </Context.Consumer>
+    <>
+      <UserForm onSubmit={onSubmitRegister} error={errorMsgRegister} title='Registrate' disabled={loadingRegister} />
+      <UserForm onSubmit={onSubmitLogin} error={errorMsgLogin} title='Iniciar sesión' disabled={loadingLoding} />
+    </>
   )
 }
